03ea9947e8f997ca90ce5e4e5dd7421f923aff6e
Islam Zoukhaiev <dev@mobeetravel.com>
2025-07-23 17:22:17 +0200

    fix

---
+++ app/components/content/product.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    v-if="productData"
+    class="rounded-(--ui-radius) overflow-hidden bg-white"
+  >
+    <img
+      class="object-cover"
+      :src="productData?.image"
+      :alt="productData?.name"
+    >
+    <div class="p-4">
+      <p class="font-bold text-lg mb-4">
+        {{ productData?.name }}
+      </p>
+      <p class="text-sm md:text-base">
+        Apport calorique
+      </p>
+      <p class="text-sm md:text-base">
+        {{ (Number(productData?.apport_calorique ?? 0)?.toLocaleString('fr-FR')) }} kcal
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  product: {
+    type: String
+  }
+})
+
+const { data: productData } = await useAsyncData(() => queryCollection('products').where('stem', '=', 'products/' + props.product).first())
+</script>
