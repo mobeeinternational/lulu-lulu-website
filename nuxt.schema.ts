404e6c24fe6ad86093e7cb0b5426980df850c26b
Islam Zoukhaiev <dev@mobeetravel.com>
2025-07-23 16:29:27 +0200

    fix

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -3,8 +3,28 @@ import { field, group } from '@nuxt/content/preview'
 export default defineNuxtSchema({
   appConfig: {
     ui: field({
-      type: 'object'
-
+      type: 'object',
+      fields: {
+        colors: field({
+          type: 'object',
+          fields: {
+            primary: field({
+              type: 'string',
+              title: 'Primary',
+              description: 'Primary color of your UI.',
+              icon: 'i-mdi-palette-outline',
+              default: 'green'
+            }),
+            neutral: field({
+              type: 'string',
+              title: 'Neutral',
+              description: 'Neutral color of your UI.',
+              icon: 'i-mdi-palette-outline',
+              default: 'slate'
+            })
+          }
+        })
+      }
     })
   }
 })
