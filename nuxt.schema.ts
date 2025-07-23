1709d51e8c68f255ca3f45c61d10e1ed74bbcc2d
Islam Zoukhaiev <dev@mobeetravel.com>
2025-07-23 15:38:30 +0200

    fix

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -1,27 +1,37 @@
-import { field, } from '@nuxt/content/preview'
+import { field, group } from '@nuxt/content/preview'
 
 export default defineNuxtSchema({
   appConfig: {
-    ui: {
-      colors: {
-        primary: field({
-          type: 'string',
-          title: 'Primary',
-          description: 'Primary color of your UI.',
+    ui: group({
+      title: 'UI',
+      description: 'UI Customization.',
+      icon: 'i-mdi-palette-outline',
+      fields: {
+        colors: group({
+          title: 'Colors',
+          description: 'Manage main colors of your application',
           icon: 'i-mdi-palette-outline',
-          default: 'green',
-          required: ['red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose']
-        }),
-        neutral: field({
-          type: 'string',
-          title: 'Neutral',
-          description: 'Neutral color of your UI.',
-          icon: 'i-mdi-palette-outline',
-          default: 'slate',
-          required: ['slate', 'gray', 'zinc', 'neutral', 'stone']
+          fields: {
+            primary: field({
+              type: 'string',
+              title: 'Primary',
+              description: 'Primary color of your UI.',
+              icon: 'i-mdi-palette-outline',
+              default: 'green',
+              required: ['red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose']
+            }),
+            neutral: field({
+              type: 'string',
+              title: 'Neutral',
+              description: 'Neutral color of your UI.',
+              icon: 'i-mdi-palette-outline',
+              default: 'slate',
+              required: ['slate', 'gray', 'zinc', 'neutral', 'stone']
+            })
+          }
         })
       }
-    }
+    })
   }
 })
 
