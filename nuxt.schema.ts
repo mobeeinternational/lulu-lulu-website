ba191efa78b047182c7b60643d444561a627abe5
Islam Zoukhaiev <dev@mobeetravel.com>
2025-07-24 16:21:01 +0200

    fix

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -4,9 +4,31 @@ export default defineNuxtSchema({
   appConfig: {
     ui: group({
       title: 'UI',
+      description: 'UI Customization.',
+      icon: 'i-mdi-palette-outline',
       fields: {
         colors: group({
-          type: 'object'
+          title: 'Colors',
+          description: 'Manage main colors of your application',
+          icon: 'i-mdi-palette-outline',
+          fields: {
+            primary: field({
+              type: 'string',
+              title: 'Primary',
+              description: 'Primary color of your UI.',
+              icon: 'i-mdi-palette-outline',
+              default: 'green',
+              required: ['red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose']
+            }),
+            neutral: field({
+              type: 'string',
+              title: 'Neutral',
+              description: 'Neutral color of your UI.',
+              icon: 'i-mdi-palette-outline',
+              default: 'slate',
+              required: ['slate', 'gray', 'zinc', 'neutral', 'stone']
+            })
+          }
         })
       }
     })
